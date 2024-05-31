2a9a76e1681489791e11ea3f4d8daf7899eff8c4
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2024-05-31 10:29:38 -0300

    Adicionado BeforeEach

--- backend/src/test/java/dev/gabrielbarbosa/DSCatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/dev/gabrielbarbosa/DSCatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package dev.gabrielbarbosa.DSCatalog.repositories;
 
 import dev.gabrielbarbosa.DSCatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,12 +15,17 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
 
+    private Long exintingId;
+
+    @BeforeEach
+    void setUp() {
+        exintingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        Long id = 1L;
-
-        productRepository.deleteById(id);
-        Optional<Product> result = productRepository.findById(id);
+        productRepository.deleteById(exintingId);
+        Optional<Product> result = productRepository.findById(exintingId);
 
         Assertions.assertFalse(result.isPresent());
     }
