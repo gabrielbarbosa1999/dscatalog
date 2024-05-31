c894e1ceec9c954a3069438e22cd662544d4b5f6
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2024-05-31 10:42:48 -0300

    Criado mas teste da repository do product

--- backend/src/test/java/dev/gabrielbarbosa/DSCatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/dev/gabrielbarbosa/DSCatalog/repositories/ProductRepositoryTests.java
@@ -1,5 +1,6 @@
 package dev.gabrielbarbosa.DSCatalog.repositories;
 
+import dev.gabrielbarbosa.DSCatalog.entities.Category;
 import dev.gabrielbarbosa.DSCatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @DataJpaTest
@@ -16,10 +18,24 @@ public class ProductRepositoryTests {
     private ProductRepository productRepository;
 
     private Long exintingId;
+    private Long countTotalProducts;
 
     @BeforeEach
     void setUp() {
         exintingId = 1L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        product.setId(null);
+
+        product = productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
     }
 
     @Test
@@ -27,6 +43,18 @@ public class ProductRepositoryTests {
         productRepository.deleteById(exintingId);
         Optional<Product> result = productRepository.findById(exintingId);
 
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void findByIdShouldIsPresent() {
+        Optional<Product> result = productRepository.findById(exintingId);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void findByIdShouldIsNotPresent() {
+        Optional<Product> result = productRepository.findById(countTotalProducts + 1);
         Assertions.assertFalse(result.isPresent());
     }
 
